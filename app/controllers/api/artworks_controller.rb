8c9db5249d962d79dbfe9d6bcc7ec04943986116
kev112 <werewaswas@live.com>
2019-01-02 01:03:37 -0800

    add create and update action

--- app/controllers/api/artworks_controller.rb
+++ app/controllers/api/artworks_controller.rb
@@ -10,14 +10,30 @@ class Api::ArtworksController < ApplicationController
     end
 
     def create
-        
+        @artwork = Artwork.new(artwork_params)
+        if @artwork.save
+            render json: @artwork
+        else
+            render json: { message: 'Error' }, status: 400
+        end
     end
 
     def update
-        
+        @artwork = Artwork.find(params[:id])
+        if @artwork.update(artwork_params)
+            render json: @artwork
+        else
+            render json: { message: 'Error' }, status: 400
+        end
     end
 
     def destroy
         
+    end
+
+    private
+
+    def artwork_params
+        params.require(:artwork).permit(:title, :description, :url)
     end
 end
