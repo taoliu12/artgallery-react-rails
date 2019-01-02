0d7e1ef4643e3dad3ebcd17a9e143e043c8fee35
kev112 <werewaswas@live.com>
2019-01-01 19:00:56 -0800

    make artworks migraation

---
+++ db/migrate/20190102024624_create_artworks.rb
@@ -0,0 +1,11 @@
+class CreateArtworks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artworks do |t|
+      t.string :title 
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
