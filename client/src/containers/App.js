eb8dfd37c0e52957cee2c6be475a769231a8b06d
kev112 <werewaswas@live.com>
2019-01-02 14:58:00 -0800

    add Artworks container component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Artworks from './Artworks';
+
+const artworks = [
+    {
+        title: 'a',
+        description: 'b',
+        url: 'c'
+    },
+    {
+        title: 'b',
+        description: 'b',
+        url: 'c'
+    },
+];
+
+
 
 class App extends Component {
     render() {
         return (
+            
             <div className='App'>
-                App 
+                App Container
+                <Artworks artworks={artworks}/>
             </div>
         )
     }
 }
 
-export default App
+export default App;
