94b838ba3e3e88197e3868c170360e12ee16bdb1
kev112 <werewaswas@live.com>
2019-01-02 20:21:30 -0800

    display styling for artwork cards

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,12 +6,17 @@ const artworks = [
     {
         title: 'a',
         description: 'b',
-        url: 'c'
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/687px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
     },
     {
         title: 'b',
         description: 'b',
-        url: 'c'
+        url: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/BioShock_cover.jpg/220px-BioShock_cover.jpg'
+    },
+    {
+        title: 'b',
+        description: 'b',
+        url: 'https://ahq-assets.s3.amazonaws.com/images/promo/me-andromeda-promo-3.jpg'
     },
 ];
 
--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './Artworks.css';
 
 const Artworks = (props) => (
-    <div>
+    <div className='ArtworksContainer'>
+        <h3>Artworks</h3>
         {props.artworks.map(artwork =>  
-            <div>
+            <div className='ArtworksCard'>
                 <h3>{artwork.title}</h3>
+                <p>{artwork.description}</p>
+                <img className='ArtworkImage' src={artwork.url} />
             </div>
         )}
     </div>
