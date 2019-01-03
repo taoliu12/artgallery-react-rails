372e59ea9be647753b93fdb287888bb89b2d186a
kev112 <werewaswas@live.com>
2019-01-03 12:39:27 -0800

    change fetch port

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Artworks from './Artworks';
 
-const artworks = [
-    {
-        title: 'a',
-        description: 'b',
-        url: 'https://www.gamersdecide.com/sites/default/files/styles/news_images/public/content-images/news/2017/04/27/final-fantasy-15-pc-square-enix-director-wants-bring-ff15-pc/ff15_pc.jpg'
-    },
-    {
-        title: 'b',
-        description: 'b',
-        url: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/BioShock_cover.jpg/220px-BioShock_cover.jpg'
-    },
-    {
-        title: 'b',
-        description: 'b',
-        url: 'https://ahq-assets.s3.amazonaws.com/images/promo/me-andromeda-promo-3.jpg'
-    },
-];
-
 
 
 class App extends Component {
@@ -33,10 +15,9 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('http://192.168.1.23:3000/api/artworks')
+        fetch('http://192.168.1.23:3001/api/artworks')
         .then(response => response.json())
-        .then(artworks => this.setState( { artworks } ));
-        console.log(this.state);
+        .then(artworks => this.setState({ artworks }));
     }
 
     render() {
@@ -44,7 +25,7 @@ class App extends Component {
             
             <div className='App'>
                 App Container
-                <Artworks artworks={artworks}/>
+                <Artworks artworks={this.state.artworks}/>
             </div>
         )
     }
