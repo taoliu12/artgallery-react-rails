9b096e761204e398c5d0c23eea54f4c7292b5699
kev112 <werewaswas@live.com>
2019-01-03 15:17:36 -0800

    add env file, working

--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './Artworks.css';
+import ArtworkCard from "../components/ArtworkCard";
 
 const Artworks = (props) => (
     <div className='ArtworksContainer'>
         <h3>Artworks</h3>
-        {props.artworks.map(artwork =>  
-            <div className='ArtworksCard'>
-                <h3>{artwork.title}</h3>
-                <p>{artwork.description}</p>
-                <img className='ArtworkImage' src={artwork.url} />
-            </div>
+        {props.artworks.map((artwork) => (
+            <ArtworkCard artwork={artwork} />
+            )
         )}
     </div>
 );
