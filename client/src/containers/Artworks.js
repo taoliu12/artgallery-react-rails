ef3495d830f3e46884df6075c63540d89b4a8140
kev112 <werewaswas@live.com>
2019-02-04 20:22:48 -0800

    add ArtworkForm

--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './Artworks.css';
 import ArtworkCard from "../components/ArtworkCard";
+import ArtworkForm from './ArtworkForm';
 
 const Artworks = (props) => (
-    <div className='ArtworksContainer'>
-        <h3>Artworks</h3>
-        {props.artworks.map((artwork) => (
-            <ArtworkCard artwork={artwork} />
-            )
-        )}
+    <div>
+        <div className='ArtworksContainer'>
+            <h3>Artworks</h3>
+            {props.artworks.map((artwork) => (
+                <ArtworkCard artwork={artwork} />
+                )
+            )}
+            
+            
+        </div>
+        <ArtworkForm />
     </div>
 );
 
