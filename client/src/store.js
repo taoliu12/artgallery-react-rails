dde133eda63ed9e18b73e0311a29701aacce3bb0
kev112 <werewaswas@live.com>
2019-02-07 21:15:13 -0800

    refactor middleware/thunk

--- client/src/store.js
+++ client/src/store.js
@@ -9,12 +9,23 @@ import thunk from 'redux-thunk';
 // import artworks from './reducers/artworks';
 // import artworkFormData from './reducers/artworkFormData';
 
+const artworksReducer = (state = [], action) => {
+    switch(action.type) {
+        case: 'ADD_ARTWORK'
+            return action.artworks
+        default: 
+            return state
+    }
+}
+
 const reducers = combineReducers({
-    
+    artworks: artworksReducer,
 })
+
+const middleware = [thunk];
 
 export default createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk),
+    applyMiddleware(...middleware),
 );
