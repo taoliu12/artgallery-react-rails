c8496e4961daa1ecf598c9e317654bdc4e455c9f
kev112 <werewaswas@live.com>
2019-02-08 11:14:32 -0800

    add default state argument in reducer

--- client/src/reducers/artworkFormData.js
+++ client/src/reducers/artworkFormData.js
@@ -1,5 +1,13 @@
-export default (state, action) => {
-    switch() {
-        
+export default (state = {
+    description: '',
+    id: null,
+    title: '',
+    url: '',
+}, action) => {
+    switch(action.type) {
+        case 'ADD_ARTWORK':
+            return action.artworkFormData;
+        default:
+            return state;
     }
 }
