2a9671d3ff949a530197c451d0af9f7b7ecd4b55
kev112 <werewaswas@live.com>
2019-02-08 10:18:44 -0800

    make routes restful

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
     return (
     <div className="navbar">
-        <NavLink to="/">Artworks</NavLink>
+        <NavLink to="/artworks">Artworks</NavLink>
         <span> | </span>
-        <NavLink to="/new">Submit</NavLink>
+        <NavLink to="artworks/new">Submit</NavLink>
     </div>
 )}
 
