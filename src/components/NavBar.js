d409ac7dfcb97b1b57b0c871d19ba521656c5d16
kev112 <werewaswas@live.com>
2019-02-08 09:37:35 -0800

    display router NavBar artworks link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {(
-    <div>
-        <NavLink />
+const NavBar = () => {
+    return (
+    <div className="navbar">
+        <NavLink to="/">Artworks</NavLink>
     </div>
-})
+)}
+
+export default NavBar;
