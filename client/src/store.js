686d3372e2aeace5311ab5552807c9190e2516c1
kev112 <werewaswas@live.com>
2019-02-08 11:22:51 -0800

    refactor store for easier reading

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,6 @@ import {
     applyMiddleware,
     combineReducers
   } from 'redux';
-
 import thunk from 'redux-thunk';
   
 import artworks from './reducers/artworks';
@@ -16,9 +15,10 @@ const reducers = combineReducers({
 
 const middleware = [thunk];
 
-export default createStore(
+const store = createStore(
     reducers,    
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(...middleware),
-
 );
+
+export default store;
