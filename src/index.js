1b606ee598209e9bd4253107e3201f446a2fe426
kev112 <werewaswas@live.com>
2019-02-07 22:00:09 -0800

    import store, fix typos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import store from './store'
+
+console.log(store)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
