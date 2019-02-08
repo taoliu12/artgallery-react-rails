dfb421333b3486da6b1cc7a9d395d7ec975ea46d
kev112 <werewaswas@live.com>
2019-02-07 19:59:23 -0800

    add initial store

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import { 
+    createStore, 
+    applyMiddleware,
+    combineReducers
+  } from 'redux';
+
+import thunk from 'redux-thunk';
+  
+// import artworks from './reducers/artworks';
+// import artworkFormData from './reducers/artworkFormData';
+
+const reducers = combineReducers({
+    
+})
+
+export default createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk),
+);
