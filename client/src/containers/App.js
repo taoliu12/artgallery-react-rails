5fe4678fabd57d492045b0538b5a3ab90685f634
kev112 <werewaswas@live.com>
2019-02-08 01:44:44 -0800

    refactor artwork form

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Artworks from './Artworks';
+import ArtworkForm from './ArtworkForm';
 
 class App extends Component {
     
@@ -15,7 +16,7 @@ class App extends Component {
     render() {
         return (
             <div className='App'>
-                App Container
+                <ArtworkForm />
                 <Artworks artworks={this.state.artworks}/>
             </div>
         )
