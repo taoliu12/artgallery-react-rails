2782ef97dca486b906c1cd8ff1408222b006e599
kev112 <werewaswas@live.com>
2019-02-08 11:33:14 -0800

    try to fix redux devtool

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk';
 import artworks from './reducers/artworks';
 import artworkFormData from './reducers/artworkFormData';
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
     artworks: artworks,
     artworkFormData: artworkFormData
 })
@@ -16,9 +16,18 @@ const reducers = combineReducers({
 const middleware = [thunk];
 
 const store = createStore(
-    reducers,    
+    rootReducer, 
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(...middleware),
 );
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(
+//     applyMiddleware(thunk),
+//   )
+// );
 
 export default store;
