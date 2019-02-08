e59bbdef5c86ff53194be270cddabb0cb4a36201
kev112 <werewaswas@live.com>
2019-02-08 09:50:58 -0800

    add router NavLink: submit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,6 +5,8 @@ const NavBar = () => {
     return (
     <div className="navbar">
         <NavLink to="/">Artworks</NavLink>
+        <span> | </span>
+        <NavLink to="/new">Submit</NavLink>
     </div>
 )}
 
