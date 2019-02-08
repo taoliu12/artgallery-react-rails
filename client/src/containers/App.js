477c2adce2aee0357d121e743f99ccd91f6158e3
kev112 <werewaswas@live.com>
2019-02-08 09:37:35 -0800

    display router NavBar artworks link

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Artworks from './Artworks';
 import ArtworkForm from './ArtworkForm';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 class App extends Component {
     
@@ -20,6 +21,7 @@ class App extends Component {
                 <h1>React Art Gallery</h1>
                 <Router>
                     <React.Fragment>
+                        <NavBar />
                         <Route exact path='/' component={Artworks} />
                         <Route exact path='/new' component={ArtworkForm} />
                     </React.Fragment>
