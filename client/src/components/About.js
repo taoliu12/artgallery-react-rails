4e445a0671b6a2fd7e58e4023bdf1bdc90494202
kev112 <werewaswas@live.com>
2019-02-09 17:46:28 -0800

    add about text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 const About = () => { 
     return (
-        <div></div>
+        <div>
+            <p>A community built for Artists. React Art Gallery is a community that was shaped with the goal of empowering artists and creating opportunities for success. It's the place for artists to connect with each other and showcase their work with others.</p>
+        </div>
     )
 }
 
