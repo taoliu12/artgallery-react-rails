66f878a36b7027fedeef949748ed5ddefe930587
kev112 <werewaswas@live.com>
2019-02-10 19:20:41 -0800

    get nested artwork route to work

--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './Artworks.css';
 import ArtworkCard from "../components/ArtworkCard";
 import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 import { getArtworks } from '../actions/artworks';
+import ArtworkShow from './ArtworkShow';
 
 class Artworks extends Component {
 
@@ -13,10 +15,14 @@ class Artworks extends Component {
     render() {
         return (
             <div>
+                <Route path='/artworks/:id' component={ArtworkShow} />
+                <h3>Test</h3>
                 <div className='ArtworksContainer'>
                     <h3>Artworks</h3>
                     {this.props.artworks.map((artwork) => (
+
                         <ArtworkCard artwork={artwork} />
+
                         )
                     )}
                 </div>
