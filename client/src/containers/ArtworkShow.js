deafc4b72769de57377892e10188a3b5e5814511
kev112 <werewaswas@live.com>
2019-02-11 12:27:56 -0800

    try to fix artwork show

--- client/src/containers/ArtworkShow.js
+++ client/src/containers/ArtworkShow.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ArtworkCard from "../components/ArtworkCard";
+import { getArtworks } from '../actions/artworks';
 
-export default class ArtworkShow extends Component {
+class ArtworkShow extends Component {
+
+    componentDidMount() {
+        this.props.getArtworks();
+    }
+
     render() { 
-        // debugger
+        console.log(this.props.artworks)
+        let artwork
+        const id = this.props.match.params.id
+        if (this.props.artworks.length > 0 )    {
+            debugger
+            artwork = this.props.artworks.find(function(element) {               
+                return element.id === id;
+            });
+        }
+        debugger
         return (
             <div className='ArtworkShow'>
                 <h3>ArtworkShow</h3>
                 <h3>{this.props.match.params.id}</h3>
+                <p>{artwork.description}</p>
                 {/* <p>{this.props.artworks[1].description}</p> */}
+                
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+    artworks: state.artworks
+})
+
+const mapDispatchToProps = ({
+    getArtworks
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtworkShow);
