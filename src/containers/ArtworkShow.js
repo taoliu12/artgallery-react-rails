2c67399455a73424a003a3ff756e5b0bf4e5964e
kev112 <werewaswas@live.com>
2019-02-10 23:51:01 -0800

    try to fix show page

--- src/containers/ArtworkShow.js
+++ src/containers/ArtworkShow.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 export default class ArtworkShow extends Component {
-    render() {
+    render() { 
+        // debugger
         return (
             <div className='ArtworkShow'>
                 <h3>ArtworkShow</h3>
                 <h3>{this.props.match.params.id}</h3>
-                {/* <p>{this.props.artworks[this.props.match.params.id].description}</p> */}
+                {/* <p>{this.props.artworks[1].description}</p> */}
             </div>
         )
     }
