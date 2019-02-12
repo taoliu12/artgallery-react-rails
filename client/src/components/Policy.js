3bc2fdab0aff72a13329c9ff20a43c033dd94737
kev112 <werewaswas@live.com>
2019-02-12 10:50:00 -0800

    add policy

---
+++ client/src/components/Policy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Policy = () => { 
+    return (
+        <div>
+            <h4>Submission Policy </h4>
+            <p>It is our policy to welcome artists of all levels from amateur to professional and to that end we do not attach any minimum standards of quality. </p>
+        </div>
+
+    )
+}
+
+export default Policy;
