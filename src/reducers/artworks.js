3d94b44b4140736f0399cc5fbeea428110e94954
kev112 <werewaswas@live.com>
2019-02-11 19:41:15 -0800

    clean up code

--- src/reducers/artworks.js
+++ src/reducers/artworks.js
@@ -4,14 +4,11 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-    // debugger
-    // console.log(action.type)
     switch(action.type) {
         case "LOADING_ARTWORKS":
             return {...state, loading: true}
         case 'GET_ARTWORKS':
             return {loading: false, artworks: action.artworks};
-            // return action.artworks;
         case 'ADD_ARTWORK':
             return state.artworks.concat(action.artwork);
 
