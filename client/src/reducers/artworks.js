b8acad6d536a7804a5b974b5d1e862fc1719ff0c
kev112 <werewaswas@live.com>
2019-02-11 16:24:27 -0800

    change artworks state into object

--- client/src/reducers/artworks.js
+++ client/src/reducers/artworks.js
@@ -1,11 +1,18 @@
-export default (state = [], action) => {
+const initialState = {
+    loading: false,
+    artworks: []
+}
+
+export default (state = initialState, action) => {
     // debugger
     // console.log(action.type)
     switch(action.type) {
+        
         case 'GET_ARTWORKS':
-            return action.artworks;
+            return {...state, artworks: action.artworks};
+            // return action.artworks;
         case 'ADD_ARTWORK':
-            return state.concat(action.artwork);
+            return state.artworks.concat(action.artwork);
 
         default: 
             return state;
