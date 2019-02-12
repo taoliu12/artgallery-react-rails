d6c8fb748d14cdbfe4d56b045eab2d20d10ca611
kev112 <werewaswas@live.com>
2019-02-11 22:55:44 -0800

    make show page work

--- client/src/reducers/artworks.js
+++ client/src/reducers/artworks.js
@@ -9,9 +9,10 @@ export default (state = initialState, action) => {
         case "LOADING_ARTWORKS":
             return {...state, loading: true}
         case 'GET_ARTWORKS':
-            return {loading: false, artworks: action.artworks};
-        case 'GET_ARTWORK':
-                return {loading: false, artwork: action.artwork};
+            return {...state, artworks: action.artworks};
+        case 'GET_ARTWORK_SHOW':
+                debugger
+                return {...state, artwork: action.artwork};
         case 'ADD_ARTWORK':
             // debugger
             return {...state, artworks: state.artworks.concat(action.artwork)}
