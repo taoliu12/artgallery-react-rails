69a0b98b6fea2a3f129bffe5ba8e800f2c4d2a68
kev112 <werewaswas@live.com>
2019-02-12 12:34:59 -0800

    add vote count

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 export default class ArtworkCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            voteCount: 0
+         }
+    }
+
+    handleClick(e) {
+        this.setState({
+            voteCount: this.state.voteCount + 1
+        })
+    }
 
     render() {
         return (
             <div className='ArtworkCard'>
+                <button onClick={this.handleClick}>Vote</button>
+                <p>{this.state.voteCount}</p>
                 <h3>{this.props.artwork.title}</h3>
                 <p>{this.props.artwork.description}</p>
                 <Link 
