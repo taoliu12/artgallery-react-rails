e02de4f06fcc02c01f4894321a706e98a0313789
kev112 <werewaswas@live.com>
2019-02-11 16:24:27 -0800

    change artworks state into object

--- src/containers/ArtworkShow.js
+++ src/containers/ArtworkShow.js
@@ -5,22 +5,18 @@ import { getArtworks } from '../actions/artworks';
 
 class ArtworkShow extends Component {
 
-    componentDidMount() {
-        // this.props.getArtworks();
-    }
+    id = parseInt(this.props.match.params.id)
+
+    artwork = this.props.artworks.find((element) => {               
+        return element.id === this.id;
+    });
 
     render() { 
-        console.log(this.props.artworks)
-        // const id = this.props.match.params.id
-        // if (this.props.artworks.length > 0 )    {
-        //     debugger
-        //     artwork = this.props.artworks.find(function(element) {               
-        //         return element.id === id;
-        //     });
-        // }
-        // debugger
+        console.log(this.artwork)
         return (
             <div className='ArtworkShow'>
+                {/* <p>{this.artwork.id}</p> */}
+
                 <p>{this.props.match.params.id}</p>
                 <h3>{ this.props.artworks[parseInt(this.props.match.params.id) - 1] ? <p>{this.props.artworks[parseInt(this.props.match.params.id) - 1].title}</p> : null }</h3>
                 
@@ -33,7 +29,7 @@ class ArtworkShow extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    artworks: state.artworks
+    artworks: state.artworks.artworks
 })
 
 const mapDispatchToProps = ({
