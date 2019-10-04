b91b8cd547b6392ddda7818ee6dc5797e915ef5d
taoliu12 <taoliudesign@gmail.com>
2019-10-04 13:05:38 -0700

    add seeds, and remove vote feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artwork.create(title: 'Luke', description: "Homage to my favorite hero", url: 'https://i.imgur.com/faCkQR2.jpg')
+Artwork.create(title: 'Yoda', description: "Homage to my favorite hero", url: 'https://i.pinimg.com/originals/b3/7d/fb/b37dfb28dcb6063a81288dadbc01a63a.jpg')
+Artwork.create(title: 'Han', description: "Homage to my favorite hero", url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f6be1286-1b5b-406e-a4e6-ba82886c3597/d7v1fqx-fd123bf2-08ec-42d0-8536-f460c4bac3fc.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y2YmUxMjg2LTFiNWItNDA2ZS1hNGU2LWJhODI4ODZjMzU5N1wvZDd2MWZxeC1mZDEyM2JmMi0wOGVjLTQyZDAtODUzNi1mNDYwYzRiYWMzZmMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.CDcWeIW5NBYLl57zJfTYq3ricZDrTD_2qLhxUBj-HCs')
