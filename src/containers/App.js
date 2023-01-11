a22f2167c62f71d090b6f59ba20fddcc03396d8d
taoliu12 <taoliudesign@gmail.com>
2023-01-11 11:25:00 -0800

    add dark mode SCSS WIP

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import './App.scss';
 import Artworks from './Artworks';
 
 import ArtworkForm from './ArtworkForm';
@@ -10,25 +10,72 @@ import ArtworkShow from './ArtworkShow';
 
 import Challenges from './Challenges'; 
 
-class App extends Component {
-    render() {
-         return (
-             <div className='App'>                 
-                <h1>React Art Gallery</h1>
-                <Router>
-                    <React.Fragment>
-                        <NavBar />
-                        <Route exact path='/' component={Artworks} />
-                        <Route exact path='/artworks' component={Artworks} />
-                        <Route exact path='/artwork/new' component={ArtworkForm} />
-                        <Route path='/artworks/:id'  component={ArtworkShow} />
-                        <Route exact path='/about' component={About} />
-                        <Route exact path='/challenges' component={Challenges} />
-                    </React.Fragment>
-                </Router>
-            </div>
-        )
-    }
-}
+function Header() {
+    const [ darkMode, setDarkMode ] = React.useState(false)
+     
+    React.useEffect(() => {
+      const body = document.body
+      const toggle = document.querySelector('.toggle-inner')
+      
+      // If dark mode is enabled - adds classes to update dark-mode styling.
+      // Else, removes and styling is as normal.
+      if( darkMode === true ) {
+        body.classList.add('dark-mode')
+        toggle.classList.add('toggle-active')
+      } else {
+        body.classList.remove('dark-mode')
+        toggle.classList.remove('toggle-active')
+      }
+    }, [darkMode])
+    
+    return (
+      <header>
+        <div
+          id="toggle"
+          onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)}
+        >
+          <div className="toggle-inner"/>
+        </div>
+      </header>
+    )
+  }
+  
+  function App() {
+    return (
+      <main>
+        <Header />
+        <div id="container">
+          <h1>Simple React Dark Mode.</h1>
+          <p>Uses state to set a class on the body if dark mode is enabled.</p>
+        </div>
+      </main>
+    )
+  }
+  
+//   ReactDOM.render(
+//     <App />,
+//     document.getElementById('app')
+//   )
+  
+// class App extends Component {
+//     render() {
+//          return (
+//              <div className='App'>                 
+//                 <h1>React Art Gallery</h1>
+//                 <Router>
+//                     <React.Fragment>
+//                         <NavBar />
+//                         <Route exact path='/' component={Artworks} />
+//                         <Route exact path='/artworks' component={Artworks} />
+//                         <Route exact path='/artwork/new' component={ArtworkForm} />
+//                         <Route path='/artworks/:id'  component={ArtworkShow} />
+//                         <Route exact path='/about' component={About} />
+//                         <Route exact path='/challenges' component={Challenges} />
+//                     </React.Fragment>
+//                 </Router>
+//             </div>
+//         )
+//     }
+// }
 
 export default App;
