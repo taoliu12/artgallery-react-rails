07d503ad4a09f4183ffa865e92076dc18c77a610
taoliu12 <taoliudesign@gmail.com>
2023-01-10 18:10:16 -0800

    adjust card css

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,8 @@
 
 Artwork.create(title: 'Luke', author: 'Joe', description: "Homage to my favorite hero", url: 'https://i.imgur.com/faCkQR2.jpg')
 Artwork.create(title: 'Yoda', author: 'Joe',  description: "Homage to my favorite hero", url: 'https://i.pinimg.com/originals/b3/7d/fb/b37dfb28dcb6063a81288dadbc01a63a.jpg')
+Artwork.create(title: 'Luke', author: 'Joe', description: "Homage to my favorite hero", url: 'https://i.imgur.com/faCkQR2.jpg')
+Artwork.create(title: 'Yoda', author: 'Joe',  description: "Homage to my favorite hero", url: 'https://i.pinimg.com/originals/b3/7d/fb/b37dfb28dcb6063a81288dadbc01a63a.jpg')
+Artwork.create(title: 'Luke', author: 'Joe', description: "Homage to my favorite hero", url: 'https://i.imgur.com/faCkQR2.jpg')
+Artwork.create(title: 'Yoda', author: 'Joe',  description: "Homage to my favorite hero", url: 'https://i.pinimg.com/originals/b3/7d/fb/b37dfb28dcb6063a81288dadbc01a63a.jpg')
 Artwork.create(title: 'Han', author: 'Joe',  description: "Homage to my favorite hero", url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f6be1286-1b5b-406e-a4e6-ba82886c3597/d7v1fqx-fd123bf2-08ec-42d0-8536-f460c4bac3fc.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y2YmUxMjg2LTFiNWItNDA2ZS1hNGU2LWJhODI4ODZjMzU5N1wvZDd2MWZxeC1mZDEyM2JmMi0wOGVjLTQyZDAtODUzNi1mNDYwYzRiYWMzZmMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.CDcWeIW5NBYLl57zJfTYq3ricZDrTD_2qLhxUBj-HCs')
