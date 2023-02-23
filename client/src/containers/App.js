e899f3c6cce55e9ca3cd2e587359512f34133214
Tao Liu <werewaswas@live.com>
2023-02-23 13:54:14 -0800

    new toggle button

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component, useState, useEffect } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+
 import './App.scss';
 import Artworks from './Artworks';
-
-import ArtworkForm from './ArtworkForm';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import About from '../components/About';
@@ -15,31 +16,25 @@ function DarkModeButton() {
     
     React.useEffect(() => {
       const body = document.body
-      const toggle = document.querySelector('.toggle-inner')
+      // const toggle = document.querySelector('.toggle-inner')
       
       // If dark mode is enabled - adds classes to update dark-mode styling.
       // Else, removes and styling is as normal.
       if( darkMode === true ) {
         body.classList.add('dark-mode')
-        toggle.classList.add('toggle-active')
         setDarkModeText("Go Light")
     
       } else {
         body.classList.remove('dark-mode')
-        toggle.classList.remove('toggle-active')
         setDarkModeText("Go Dark")
       }
     }, [darkMode])
     
     return (
-      <div className='dark-mode-div'>
-            {darkModeText}
-        <div
-          id="toggle"
-          onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)}
-        >
-          <div className="toggle-inner"/>
-        </div>
+      <div className='dark-mode-div'> 
+        <IconButton sx={{ ml: 1 }} onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)} color="inherit">
+         <Brightness4Icon />
+        </IconButton>
       </div>
     )
   }
