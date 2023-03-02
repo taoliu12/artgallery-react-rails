845654167af1826ac62d4292e5ee901854e7d473
Tao Liu <werewaswas@live.com>
2023-03-02 15:35:46 -0800

    move darkmode button

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,49 +1,20 @@
 import React, { Component, useState, useEffect } from 'react';
-import IconButton from '@mui/material/IconButton';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
+
 
 import './App.scss';
 import Artworks from './Artworks';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+
 import About from '../components/About';
+
 import ArtworkShow from './ArtworkShow';
 import ArtworkForm from './ArtworkForm'; 
-
-function DarkModeButton() {
-    const [ darkMode, setDarkMode ] = React.useState(false)
-    const [ darkModeText, setDarkModeText ] = React.useState("Toggle Dark")
-    
-    React.useEffect(() => {
-      const body = document.body
-      // const toggle = document.querySelector('.toggle-inner')
-      
-      // If dark mode is enabled - adds classes to update dark-mode styling.
-      // Else, removes and styling is as normal.
-      if( darkMode === true ) {
-        body.classList.add('dark-mode')
-        setDarkModeText("Go Light")
-    
-      } else {
-        body.classList.remove('dark-mode')
-        setDarkModeText("Go Dark")
-      }
-    }, [darkMode])
-    
-    return (
-      <div className='dark-mode-div'> 
-        <IconButton sx={{ ml: 1 }} onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)} color="inherit">
-         <Brightness4Icon />
-        </IconButton>
-      </div>
-    )
-  }
 
 class App extends Component {
     render() {
          return (
-            <main>
-              <DarkModeButton />
+            <main> 
               <div className='App' id="container">                 
               <p className='site-title'>React Art Gallery</p>
                  <Router>
