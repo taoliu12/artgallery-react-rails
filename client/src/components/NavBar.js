2e2b68c47886934bf6d47997a8b2d2646dc3afbb
Tao Liu <werewaswas@live.com>
2023-03-02 15:05:19 -0800

    sticky navbar semi-working

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+    useEffect(() => {
+        // code to run after render goes here
+        console.log('navBar')
+        let nav = document.querySelector(".navbar");
+        let sticky = nav.offsetTop;
+        window.onscroll = function() {
+            sticker()};
+            
+            function sticker() {
+                console.log(sticky)
+            if (window.pageYOffset >= sticky) {
+                nav.classList.add("sticky")
+            } else {
+              nav.classList.remove("sticky");
+            }
+        }
+    });
+
     return (
-    <div className="navbar">         
-        <NavLink to="/artworks"><button>Artworks</button></NavLink>         
-        <NavLink to="/artwork/new"><button>Submit New Artwork</button></NavLink>   
-    </div>
+        <div className="navbar">         
+            <NavLink to="/artworks"><button>Artworks</button></NavLink>         
+            <NavLink to="/artwork/new"><button>Submit New Artwork</button></NavLink>   
+        </div>
 )}
 
 export default NavBar;
