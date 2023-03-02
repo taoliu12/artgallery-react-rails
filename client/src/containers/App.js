a4292e13a0cf0062047fe75cb4e2cea8418d8d6a
tao <taoliudesign@gmail.com>
2023-03-02 08:46:40 -0800

    new seed file
    wip

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import About from '../components/About';
 import ArtworkShow from './ArtworkShow';
-import ArtworkSketch from './ArtworkSketch'; 
+import ArtworkForm from './ArtworkForm'; 
 
 function DarkModeButton() {
     const [ darkMode, setDarkMode ] = React.useState(false)
@@ -51,7 +51,7 @@ class App extends Component {
                          <NavBar /><br></br>
                          <Route exact path='/' component={Artworks} />
                          <Route exact path='/artworks' component={Artworks} />
-                         <Route exact path='/artwork/new' component={ArtworkSketch} />                          
+                         <Route exact path='/artwork/new' component={ArtworkForm} />                          
                          <Route path='/artworks/:id'  component={ArtworkShow} />
                         {/* <Route exact path='/about' component={About} />
                          <Route exact path='/challenges' component={Challenges} /> */}
