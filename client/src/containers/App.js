664fb7a8b8437e7a28d0aa257946baf0610925c6
Tao Liu <werewaswas@live.com>
2023-03-02 20:43:13 -0800

    added scroll to top

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,22 +1,30 @@
 import React, { Component, useState, useEffect } from 'react';
-
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
+import PropTypes from 'prop-types';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 import './App.scss';
 import Artworks from './Artworks';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
-
+import Toolbar from '@mui/material/Toolbar';
 import About from '../components/About';
 
 import ArtworkShow from './ArtworkShow';
 import ArtworkForm from './ArtworkForm'; 
+import ScrollToTop from '../components/ScrollToTop';
 
 class App extends Component {
     render() {
          return (
             <main> 
-              <div className='App' id="container">                 
-              <p className='site-title'>React Art Gallery</p>
+              <Toolbar id="back-to-top-anchor" />
+              <div className='App' id="container">    
                  <Router>
                      <React.Fragment>
                          <NavBar /><br></br>
@@ -28,6 +36,11 @@ class App extends Component {
                          <Route exact path='/challenges' component={Challenges} /> */}
                      </React.Fragment>
                  </Router>
+               
+ 
+                 <ScrollToTop />
+      
+               
              </div>
             </main>
         )
