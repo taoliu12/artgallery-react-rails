1675b57355acdd520f6475c67769f8968fcd8b7a
Tao Liu <werewaswas@live.com>
2023-03-03 21:28:10 -0800

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'localhost:3001', 'https://artgallery-react-rails.onrender.com'
 
     resource '*',
       headers: :any,
