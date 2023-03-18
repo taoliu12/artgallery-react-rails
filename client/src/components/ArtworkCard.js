7d906516bb7dbe17225e76c719edacdfe912d086
Tao Liu <werewaswas@live.com>
2023-03-18 12:16:16 -0700

    image overflow fix

--- client/src/components/ArtworkCard.js
+++ client/src/components/ArtworkCard.js
@@ -23,12 +23,14 @@ export default class ArtworkCard extends Component {
                 <p>{this.state.voteCount}</p> */}
                 <h3>{this.props.artwork.title}</h3>
                 <p>{this.props.artwork.author}</p>
-                <Link 
-                    to={`/artworks/${this.props.artwork.id}`}
-                    onClick={() => window.scrollTo(0,0)}
-                    >
-                    <img alt={this.props.artwork.title} className='ArtworkImage' src={this.props.artwork.url} />
-                </Link>
+                <div className='card-image-div'>
+                    <Link 
+                        to={`/artworks/${this.props.artwork.id}`}
+                        onClick={() => window.scrollTo(0,0)}
+                        >
+                        <img alt={this.props.artwork.title} className='ArtworkImage' src={this.props.artwork.url} />
+                    </Link>
+                </div>
                     
             </div>
         )
