d02d9064e24ee972c9f3206d234df6efb3e7aa27
Tao Liu <taoliudesign@gmail.com>
2023-04-18 00:40:12 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :artworks, :except => [:new, :edit] #no need for new and edit routes, because react renders the respective forms
+    resources :artworks, :except => [:new, :edit] 
+    
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'  
+    get "/userInSession", to:"sessions#get_logged_in_user"
   end
 
 end
