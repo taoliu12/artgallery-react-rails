103f048356a7ba7c75eb1d33bf6752d44c70ab84
Tao Liu <taoliudesign@gmail.com>
2023-05-12 09:38:56 -0700

    Update render-build.sh
    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,6 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
+bundle exec rake db:drop
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
