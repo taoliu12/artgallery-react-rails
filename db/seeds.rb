8841360631791944f4ae009f2fd44627a8f93e1d
Tao Liu <taoliudesign@gmail.com>
2023-05-12 10:11:57 -0700

    don't seed nil url images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ artworksArray.each do |obj|
     technique = obj['technique']
     url = obj['images']&.dig('web', 'url')
         
-    Artwork.create(title: title, author: artist,  description: technique, url: url)
+    Artwork.create(title: title, author: artist,  description: technique, url: url) if url
 end
 
 
