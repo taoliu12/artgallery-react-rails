2a10c4e2fbe7d512b14c9589a6ebbbba140e4fe9
Tao Liu <taoliudesign@gmail.com>
2023-06-11 11:14:22 -0700

    Update Artworks.js

--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -81,27 +81,35 @@ function ArtworkGallery() {
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <div className='ArtworksContainer'> 
+      {searchArtworksResult.map((artwork) => {
+            console.log(artwork);
+            return (
+              <div key={artwork.id}>
+                <ArtworkCard artwork={artwork} onClick={() => handleOpenModal(artwork)} />
+                <Button onClick={handleOpen}>Open modal</Button>
+                <Modal
+                  open={selectedArtwork !== null} 
+                  onClose={handleCloseModal}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                      Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                  </Box>
+                </Modal>
+              </div>
+            );
+          })}
+        </div>
   );
 }
 
