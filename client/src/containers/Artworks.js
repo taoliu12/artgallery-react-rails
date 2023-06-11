e6d35811486de3cfcea777ca069bb5606d006c5f
Tao Liu <taoliudesign@gmail.com>
2023-06-11 10:29:20 -0700

    wip

--- client/src/containers/Artworks.js
+++ client/src/containers/Artworks.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import ArtworkCard from '../components/ArtworkCard';
 import SearchForm from './SearchForm';
+import Button from '@mui/material/Button'; 
+import Modal from '@mui/material/Modal';
+
 import './Artworks.scss';
 
 const infiniteScrollHasMoreThreshold = 19; // number of items to load after scrolling minus 1
@@ -11,7 +14,17 @@ function ArtworkGallery() {
   const [searchArtworksResult, setSearchArtworksResult] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [searchParam, setSearchParam] = useState('');
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);   
+
+  const [selectedArtwork, setSelectedArtwork] = useState(null);
+
+  const handleOpenModal = (artwork) => {
+    setSelectedArtwork(artwork);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedArtwork(null);
+  };
 
   useEffect(() => {
     console.log('useEffect loadArtworks');
@@ -51,7 +64,6 @@ function ArtworkGallery() {
     fetchSearchedArtworks(searchParam, 1);
   };
 
-
   console.log('search new artworks state', searchArtworksResult.length);
 
   return (    
@@ -61,7 +73,22 @@ function ArtworkGallery() {
         <div className='ArtworksContainer'> 
           {searchArtworksResult.map((artwork) => {
             console.log(artwork);
-            return <ArtworkCard key={`${artwork.id}-${rand()}`} artwork={artwork} />;
+            return (
+              <div key={artwork.id}>
+                <ArtworkCard artwork={artwork} onClick={() => handleOpenModal(artwork)} />
+                <Modal open={selectedArtwork} onClose={handleCloseModal} onHide={handleCloseModal}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Artwork Details</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <p>test</p>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button onClick={handleCloseModal}>Close</Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
+            );
           })}
         </div>
         <InfiniteScroll
