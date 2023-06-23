949eea0eb996f10b8ec8aea78de80ebeef4a4b53
tao liu <taoliudesign@gmail.com>
2023-06-23 14:29:42 -0700

    wip

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,42 +1,69 @@
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 
 const styles = {
   width: "100%",
-  height: '380px',
+  height: "380px",
   display: "flex",
   color: "white",
   alignItems: "center",
 };
 
-// const colors = ['#FF8C00', '#003E85', '#00502B', '#4A026D'];
-const colors = ['gray'];
+const colors = ['rgb(255, 143, 28)', '#003E85', '#00502B', '#ab1b1e'];
 
 const PostCard = ({ index = 0 }) => {
   const isEven = index % 2 === 0;
   const getColor = () => colors[index % colors.length];
 
   const textBox = (
-    <Box sx={{
-      width: '740px',
-      height: '300px',
-      backgroundColor: getColor(),
-      pX: 12,
-      pY: 13,
-    }}>
-      <h3>Film Screening:</h3>
-      <h1>Rosetta</h1>
-      <i>Kieslowki Theatre</i>
+    <Box
+      sx={{
+        width: "740px",
+        height: "100%",
+        backgroundColor: getColor(),
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        paddingX: 6,
+        paddingY: 3,
+        alignItems: 'flex-start',
+        fontWeight: 800,
+        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)',
+      }}
+      >
+      <Typography
+        sx={{
+          textAlign: 'left',
+          fontFamily: "Georgia, serif",
+          fontSize: 55,
+          letterSpacing: '-1px',
+        }}
+      >
+        Rosetta: Double Life of Veronique
+      </Typography>
+
+      <Typography
+        sx={{
+          fontFamily: "Georgia, serif",
+          fontSize: 25,
+        }}
+      >
+        Film Screening
+      </Typography>
     </Box>
   );
 
   const imageBox = (
-    <Box sx={{
-      flex: 1,
-      height: '100%',
-      backgroundImage: 'url("https://th.bing.com/th/id/OIP.aKsOu84Moq9ZSLO9fqQlbgHaEK?pid=ImgDet&rs=1")',
-      backgroundSize: 'cover',
-      backgroundPosition: 'fit',
-    }}></Box>
+    <Box
+      sx={{
+        flex: 1,
+        height: "100%",
+        backgroundImage:
+          'url("https://petapixel.com/assets/uploads/2022/08/fdfs11-800x533.jpg")',                      
+        backgroundSize: "cover",
+        backgroundPosition: "fit",
+      }}
+    ></Box>
   );
 
   return (
