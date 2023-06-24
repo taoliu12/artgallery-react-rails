4df1b9028236667b3e5cfbf727b28c6f4d923efd
tao liu <taoliudesign@gmail.com>
2023-06-24 16:44:47 -0700

    wip

---
+++ db/migrate/20230624234430_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :type
+      t.text :description
+      t.date :date
+      t.time :time
+      t.text :summary
+
+      t.timestamps
+    end
+  end
+end
