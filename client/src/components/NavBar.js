cfefc635b0426505f67fd21105f559484dfc28d7
Tao Liu <taoliudesign@gmail.com>
2023-07-30 14:12:11 -0700

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,27 @@
-import React, { useEffect} from 'react';
-import { NavLink } from 'react-router-dom';
-import DarkModeButton from '../components/DarkModeButton';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import DarkModeButton from "../components/DarkModeButton";
 // import SearchForm2 from '../containers/SearchForm2';
 
 const NavBar = () => {
-
-    return (
-        <div className="navbar">
-            <NavLink to="/artworks"><button className='nav-button'>Artworks</button></NavLink>         
-            <NavLink to="/artwork/new"><button className='nav-button'>Submit Artwork</button></NavLink>   
-            <NavLink to="/signup"><button className='nav-button'>Signup</button></NavLink>   
-            <NavLink to="/login"><button className='nav-button'>Login</button></NavLink>   
-            {/* <SearchForm2 /> */}
-            <DarkModeButton />
-        </div>
-)}
+  return (
+    <div className="navbar">
+      <NavLink to="/artworks">
+        <button className="nav-button">Artworks</button>
+      </NavLink>
+      <NavLink to="/artwork/new">
+        <button className="nav-button">Submit Artwork</button>
+      </NavLink>
+      <NavLink to="/signup">
+        <button className="nav-button">Signup</button>
+      </NavLink>
+      <NavLink to="/login">
+        <button className="nav-button">Login</button>
+      </NavLink>
+      {/* <SearchForm2 /> */}
+      <DarkModeButton />
+    </div>
+  );
+};
 
 export default NavBar;
