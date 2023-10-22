acc0001567b3a85229774e89f44203540af991a2
taoliu12 <taoliudesign@gmail.com>
2023-10-22 11:38:23 -0700

    update render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 # Build commands for front end to create the production build
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
+# rm -rf public
+# npm install --prefix client && npm run build --prefix client
+# cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
